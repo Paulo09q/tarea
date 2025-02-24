59ac39b4618eabbf281523353b27693606cff223
Usuario <Usuario@DESKTOP-FAULGD5>
2025-02-24 17:34:48 -0500

    Agregar archivos de proyecto.

---
+++ Config/ejemplodbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bodegas.Models;
+
+
+namespace Bodegas.Config
+{
+    public class ejemplodbContext:DbContext
+    {
+       
+        
+        
+        public ejemplodbContext(DbContextOptions contexto):base(contexto) {}
+
+
+       //Aqui crea las tablas
+        public DbSet<ClienteModel> Clientes { get; set; }
+    }
+}
+    
---
+++ Models/ClienteModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
+namespace Bodegas.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo es requerido")]
+        //[MaxLength(10, ErrorMessage ="La cantidad maxima es 10 caracteres")]
+        [Display(Name= "Cédula o RUC")]
+        [Length(10,13,ErrorMessage = "El valor del campo es entre 10 y 13 caracteres")]
+
+        public string Cedula_RUC { get; set; }
+        [Required(ErrorMessage = "El campo es requerido")]
+        //[MaxLength(10, ErrorMessage ="La cantidad maxima es 10 caracteres")]
+        [Display(Name = "Nombre")]
+        [Length(10, 13, ErrorMessage = "El valor del campo es entre 10 y 13 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El campo es requerido")]
+        //[MaxLength(10, ErrorMessage ="La cantidad maxima es 10 caracteres")]
+        [Display(Name = "Apellido")]
+        [Length(10, 13, ErrorMessage = "El valor del campo es entre 10 y 13 caracteres")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El campo es requerido")]
+        //[MaxLength(10, ErrorMessage ="La cantidad maxima es 10 caracteres")]
+        [Display(Name = "Dirección")]
+        [Length(10, 13, ErrorMessage = "El valor del campo es entre 10 y 13 caracteres")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "El campo es requerido")]
+        //[MaxLength(10, ErrorMessage ="La cantidad maxima es 10 caracteres")]
+        [Display(Name = "Teléfono")]
+        [Length(10, 13, ErrorMessage = "El valor del campo es entre 10 y 13 caracteres")]
+        public string Telefono { get; set; }
+        
+        public int edad { get; set; }
+        
+
+    }
+}
